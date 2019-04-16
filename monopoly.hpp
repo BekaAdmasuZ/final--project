6e3dcadb9ab80b7d8bb90cc403e56b2052b0c090
Beka Admasu <bead3035@colorado.edu>
2019-04-16 09:14:21 -0600

    made a few changes in cpp and hpp

--- monopoly.hpp
+++ monopoly.hpp
@@ -8,10 +8,11 @@ struct Property{
 }
 class Monopoly{
   public:
+    Monopoly();
+    ~Monopoly();
     int rollDice();
     void addplayer(string name);//enque
     void removeplayer(string name);//deque
-    
 
   private:
     // head of the linked list goes here
