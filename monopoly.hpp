65e0563b33d87bf7dc5cb67a62d081281343d08f
christianpolanco <49686680+christianpolanco@users.noreply.github.com>
2019-04-16 09:09:00 -0600

    commit

--- monopoly.hpp
+++ monopoly.hpp
@@ -5,27 +5,20 @@
 #include <vector>
 
 
-
-struct Player{
-  int money;
-
-}
 struct Property{
 
-}
-
-=======
+};
 
 struct Player{
   int money;
+  vector <Property *> owned;
+};
 
->>>>>>> 3d4b59357b9118621a7af3172b542cf814836e1b
-}
 
->>>>>>> 5406f6e592e4ddd016bcde78e1420d458e6efa4e
+
 class Monopoly{
   public:
     int rollDice();
     void addplayer(string name);
 
-}
+};
