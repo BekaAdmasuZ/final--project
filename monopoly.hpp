908682f2e112a69fbb1b6ee4b6d3917c482ac5b2
Beka Admasu <bead3035@colorado.edu>
2019-04-16 09:07:20 -0600

    updated the class Monopoly

--- monopoly.hpp
+++ monopoly.hpp
@@ -6,19 +6,14 @@ struct Player{
 struct Property{
 
 }
-
-=======
-
-struct Player{
-  int money;
-
->>>>>>> 3d4b59357b9118621a7af3172b542cf814836e1b
-}
-
->>>>>>> 5406f6e592e4ddd016bcde78e1420d458e6efa4e
 class Monopoly{
   public:
     int rollDice();
-    void addplayer(string name);
+    void addplayer(string name);//enque
+    void removeplayer(string name);//deque
+    
+
+  private:
+    // head of the linked list goes here
 
 }
